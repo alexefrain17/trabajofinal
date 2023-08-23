5eb61f2c86dcf50e81e3117242b2ca165f03ecd9
alexefrain17 <1432461@senati.pe>
2023-08-23 17:14:34 -0500

    agregue las primeras preguntas

--- trabajo/src/main/java/com/mycompany/trabajo/Trabajo.java
+++ trabajo/src/main/java/com/mycompany/trabajo/Trabajo.java
@@ -13,6 +13,14 @@ public class Trabajo {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
-    
+    // Pregunta 1
+    System.out.println("que es Microsoft ");
+    String respuesta1 = scanner.nextLine();
+    if (respuesta1.equalsIgnoreCase("es una compañía estadounidense vinculada al desarrollo, fabricación")) {
+        System.out.println("¡Correcto!");
+
+    }else{
+        System.out.println("Respuesta incorrecta. La respuesta correcta es una compañia.");
+    }
     }
 }
